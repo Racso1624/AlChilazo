c1b9e882fe14a2659aad13ef6804ea61882e2766
Racso1624 <fer10_lb@outlook.com>
2022-03-11 00:18:30 -0600

    Se realizo la pagina de servicios

--- project/al_chilazo/lib/main.dart
+++ project/al_chilazo/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'screens/login_screen.dart';
+import 'screens/services_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: ServicesScreen(),
     );
   }
 }
