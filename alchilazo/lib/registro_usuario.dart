4a0210483f6b5a5a29f5f98efd6910e9a793058a
Yong <yong.park123456@gmail.com>
2022-04-06 13:43:22 -0600

    trabajando en la parte de guardar los usuarios en firebase, se borro todo lo que habia de registrar para hacerlo de nuevo

--- alchilazo/lib/registro_usuario.dart
+++ alchilazo/lib/registro_usuario.dart
@@ -1,5 +1,6 @@
 import 'package:alchilazo/pantalla_home.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Registro extends StatefulWidget {
   static String id = 'registro_usuario';
@@ -9,133 +10,114 @@ class Registro extends StatefulWidget {
 }
 
 class _RegistroState extends State<Registro> {
+  final _auth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+  String _userDPI = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState?.save();
+      print(_userEmail);
+      print(_userName);
+      print(_userPassword);
+      print(_userDPI);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(
-        title: Text("Registro"),
-        backgroundColor: Colors.red,
-      ),
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20.0,
+        child: Card(
+          margin: EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty || !value.contains('@')) {
+                          return 'Ingresse un correo valido';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Correo electronico',
+                      ),
+                      onSaved: (value) {
+                        _userEmail = value!;
+                      },
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 4) {
+                          return 'el nombre de usuario debe de ser de minimo 4 caracteres';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Nombre Usuario',
+                      ),
+                      onSaved: (value) {
+                        _userName = value!;
+                      },
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 7) {
+                          return 'la contraseña debe de ser de minimo 7 caracteres';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Contraseña',
+                      ),
+                      obscureText: true,
+                      onSaved: (value) {
+                        _userPassword = value!;
+                      },
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 13) {
+                          return 'El DPI tiene 13 numeros';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'DPI',
+                      ),
+                      onSaved: (value) {
+                        _userDPI = value!;
+                      },
+                    ),
+                    SizedBox(height: 12),
+                    RaisedButton(
+                      child: Text('Login'),
+                      onPressed: _trySubmit,
+                    ),
+                  ],
+                ),
+              ),
             ),
-            _nameTextField(),
-            SizedBox(
-              height: 20.0,
-            ),
-            _emailTextField(),
-            SizedBox(
-              height: 20.0,
-            ),
-            _passwordTextField(),
-            SizedBox(
-              height: 20.0,
-            ),
-            _dpiTextField(),
-            SizedBox(
-              height: 25.0,
-            ),
-            _buttonRegistrar(),
-          ],
+          ),
         ),
       ),
-    ));
-  }
-
-  Widget _nameTextField() {
-    return StreamBuilder(
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-      return Container(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: TextField(
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            icon: Icon(Icons.abc),
-            hintText: 'Ejemplo: Juan/Juana',
-            labelText: 'Nombre',
-          ),
-          //Variable que recibirá el correo
-          onChanged: (value) {},
-        ),
-      );
-    });
-  }
-
-  Widget _emailTextField() {
-    return StreamBuilder(
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-      return Container(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: TextField(
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            icon: Icon(Icons.email),
-            hintText: 'ejemplo@correo.com',
-            labelText: 'Correo electronico',
-          ),
-          //Variable que recibirá el correo
-          onChanged: (value) {},
-        ),
-      );
-    });
-  }
-
-  Widget _passwordTextField() {
-    return StreamBuilder(
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-      return Container(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: TextField(
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            icon: Icon(Icons.lock),
-            hintText: 'Ejemplo: 123456',
-            labelText: 'Contraseña',
-          ),
-          //Variable que recibirá el correo
-          onChanged: (value) {},
-        ),
-      );
-    });
-  }
-
-  Widget _dpiTextField() {
-    return StreamBuilder(
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-      return Container(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: TextField(
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            icon: Icon(Icons.abc),
-            hintText: 'Ejemplo: 123456789',
-            labelText: 'DPI',
-          ),
-          //Variable que recibirá el correo
-          onChanged: (value) {},
-        ),
-      );
-    });
-  }
-
-  Widget _buttonRegistrar() {
-    return StreamBuilder(
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-      return RaisedButton(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-            child: Text('Registrar'),
-          ),
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => HomePage()));
-          });
-    });
+    );
   }
 }
