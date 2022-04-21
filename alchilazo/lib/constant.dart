c3cb2bb333024c315ae624214dbd530d27c809c5
Yong <yong.park123456@gmail.com>
2022-04-21 15:30:35 -0600

    cambios para poder realizar la lectura de la tabla de trabajaodres

--- alchilazo/lib/constant.dart
+++ alchilazo/lib/constant.dart
@@ -4,3 +4,4 @@
 const MONGO_CONN_URL =
     "mongodb+srv://users:SZTCiFwkpYuQdwjS@cluster0.r7vmr.mongodb.net/Database?retryWrites=true&w=majority";
 const USER_COLLECTION = "users"; //este es el nombre de la tabala
+const WORKER_COLLECTION = "workers";
--- alchilazo/lib/mongo.dart
+++ alchilazo/lib/mongo.dart
@@ -4,12 +4,13 @@ import 'package:alchilazo/constant.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 class MongoDatabase {
-  static var db, userCollection;
+  static var db, userCollection, workerCollection;
   static connect() async {
     db = await Db.create(MONGO_CONN_URL);
     await db.open();
     inspect(db);
     userCollection = db.collection(USER_COLLECTION);
+    workerCollection = db.collection(WORKER_COLLECTION);
   }
 
   static Future<String> insert_usuario(MongoDbModel data) async {
