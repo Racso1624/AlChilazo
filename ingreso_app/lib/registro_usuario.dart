e15031b3be4ef4199d9373e0f93ac3153e309543
YongBum <59983323+Yong-Park@users.noreply.github.com>
2022-04-21 22:07:56 -0600

    subiendo cambio realizado para ingreso de datos a la base de datos

--- ingreso_app/lib/registro_usuario.dart
+++ ingreso_app/lib/registro_usuario.dart
@@ -1,5 +1,8 @@
+import 'package:alchilazo/pantalla_home.dart';
 import 'package:flutter/material.dart';
-import 'package:ingreso_app/login_page.dart';
+import 'package:mongo_dart/mongo_dart.dart' as M;
+import 'package:alchilazo/mongo.dart';
+import 'MongoDbModel_User.dart';
 
 class Registro extends StatefulWidget {
   static String id = 'registro_usuario';
@@ -9,12 +12,20 @@ class Registro extends StatefulWidget {
 }
 
 class _RegistroState extends State<Registro> {
+  //uso para obtener los datos ingreados para subirlo luego a la base de datos
+  var name = new TextEditingController();
+  var email = new TextEditingController();
+  var pass = new TextEditingController();
+  var dpi = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      appBar: AppBar(title: Text("Registro"),
-      backgroundColor: Color.fromARGB(255, 0, 0, 0),),
+      appBar: AppBar(
+        title: Text("Registro"),
+        backgroundColor: Colors.red,
+      ),
       body: Center(
         child: Column(
           children: [
@@ -34,7 +45,9 @@ class _RegistroState extends State<Registro> {
               height: 20.0,
             ),
             _dpiTextField(),
-            SizedBox(height: 25.0,),
+            SizedBox(
+              height: 25.0,
+            ),
             _buttonRegistrar(),
           ],
         ),
@@ -42,12 +55,24 @@ class _RegistroState extends State<Registro> {
     ));
   }
 
- Widget _nameTextField() {
-   return StreamBuilder(
+  Future<void> _insertData(
+      String name, String email, String pass, String dpi) async {
+    var _id = M.ObjectId();
+    final data = MongoDbModel_User(
+        id: _id, nombre: name, correo: email, password: pass, dpi: dpi);
+    var result = await MongoDatabase.insert_usuario(data);
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text("ISERTED ID " + _id.$oid)));
+    _clearAll();
+  }
+
+  Widget _nameTextField() {
+    return StreamBuilder(
         builder: (BuildContext context, AsyncSnapshot snapshot) {
       return Container(
         padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: TextField(
+          controller: name,
           keyboardType: TextInputType.emailAddress,
           decoration: InputDecoration(
             icon: Icon(Icons.abc),
@@ -59,14 +84,15 @@ class _RegistroState extends State<Registro> {
         ),
       );
     });
- }
+  }
 
- Widget _emailTextField() {
-   return StreamBuilder(
+  Widget _emailTextField() {
+    return StreamBuilder(
         builder: (BuildContext context, AsyncSnapshot snapshot) {
       return Container(
         padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: TextField(
+          controller: email,
           keyboardType: TextInputType.emailAddress,
           decoration: InputDecoration(
             icon: Icon(Icons.email),
@@ -78,14 +104,15 @@ class _RegistroState extends State<Registro> {
         ),
       );
     });
- }
+  }
 
- Widget _passwordTextField() {
-   return StreamBuilder(
+  Widget _passwordTextField() {
+    return StreamBuilder(
         builder: (BuildContext context, AsyncSnapshot snapshot) {
       return Container(
         padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: TextField(
+          controller: pass,
           keyboardType: TextInputType.emailAddress,
           decoration: InputDecoration(
             icon: Icon(Icons.lock),
@@ -97,14 +124,15 @@ class _RegistroState extends State<Registro> {
         ),
       );
     });
- }
+  }
 
- Widget _dpiTextField() {
-   return StreamBuilder(
+  Widget _dpiTextField() {
+    return StreamBuilder(
         builder: (BuildContext context, AsyncSnapshot snapshot) {
       return Container(
         padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: TextField(
+          controller: dpi,
           keyboardType: TextInputType.emailAddress,
           decoration: InputDecoration(
             icon: Icon(Icons.abc),
@@ -116,22 +144,38 @@ class _RegistroState extends State<Registro> {
         ),
       );
     });
- }
+  }
 
- Widget _buttonRegistrar() {
-   return StreamBuilder(
+  Widget _buttonRegistrar() {
+    return StreamBuilder(
         builder: (BuildContext context, AsyncSnapshot snapshot) {
       return RaisedButton(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-            child: Text('Registrar'),
-          ),
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-          });
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+          child: Text('Registrar'),
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        onPressed: () {
+          _insertData(name.text, email.text, pass.text, dpi.text);
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomePage(),
+            ),
+          );
+          print(name.text);
+          print(email.text);
+          print(pass.text);
+          print(dpi.text);
+        },
+      );
     });
- }
+  }
+
+  void _clearAll() {
+    name.text = '';
+    email.text = '';
+    pass.text = '';
+    dpi.text = '';
+  }
 }
