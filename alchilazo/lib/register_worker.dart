7c5a472a6360724d581c658fbf3907b8e2285e98
yong park <yong.park123456@gmail.com>
2022-08-18 22:57:09 -0600

    probando obtener posicion actual

--- alchilazo/lib/register_worker.dart
+++ alchilazo/lib/register_worker.dart
@@ -5,6 +5,8 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mongo_dart/mongo_dart.dart' as M;
 import 'package:flutter/material.dart';
 import 'MongoDbModel_Worker.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
 var lista_trabajos = [];
 
@@ -19,6 +21,87 @@ class RegisterWorker extends StatefulWidget {
 }
 
 class _MyRegisterWorkerState extends State<RegisterWorker> {
+  bool servicestatus = false;
+  bool haspermission = false;
+  late LocationPermission permission;
+  late Position position;
+  String long = "", lat = "";
+  late StreamSubscription<Position> positionStream;
+
+  @override
+  void initState() {
+    checkGps();
+    super.initState();
+  }
+
+  checkGps() async {
+    servicestatus = await Geolocator.isLocationServiceEnabled();
+    if (servicestatus) {
+      permission = await Geolocator.checkPermission();
+
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          print('Location permissions are denied');
+        } else if (permission == LocationPermission.deniedForever) {
+          print("'Location permissions are permanently denied");
+        } else {
+          haspermission = true;
+        }
+      } else {
+        haspermission = true;
+      }
+
+      if (haspermission) {
+        setState(() {
+          //refresh the UI
+        });
+
+        getLocation();
+      }
+    } else {
+      print("GPS Service is not enabled, turn on GPS location");
+    }
+
+    setState(() {
+      //refresh the UI
+    });
+  }
+
+  getLocation() async {
+    position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position.longitude); //Output: 80.24599079
+    print(position.latitude); //Output: 29.6593457
+
+    long = position.longitude.toString();
+    lat = position.latitude.toString();
+
+    setState(() {
+      //refresh UI
+    });
+
+    LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high, //accuracy of the location data
+      distanceFilter: 100, //minimum distance (measured in meters) a
+      //device must move horizontally before an update event is generated;
+    );
+
+    StreamSubscription<Position> positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position position) {
+      print(position.longitude); //Output: 80.24599079
+      print(position.latitude); //Output: 29.6593457
+
+      long = position.longitude.toString();
+      lat = position.latitude.toString();
+
+      setState(() {
+        //refresh UI on update
+      });
+    });
+  }
+
   int _activeStepIndex = 0;
 
   var address = TextEditingController();
@@ -179,7 +262,7 @@ class _MyRegisterWorkerState extends State<RegisterWorker> {
               width: 400.0,
               height: 500.0,
               padding: const EdgeInsets.all(20.0),
-              child: MapScreen(),
+              child: MapScreen(double.parse(long), double.parse(lat)),
             ),
           ),
         ),
@@ -318,14 +401,26 @@ class Card extends StatelessWidget {
 }
 
 class MapScreen extends StatefulWidget {
+  MapScreen(double long, double lat);
+
+  static double get lat => MapScreen.lat;
+
+  static double get long => MapScreen.long;
+
   @override
-  _MapScreenState createState() => _MapScreenState();
+  _MapScreenState createState() => _MapScreenState(long, lat);
 }
 
 class _MapScreenState extends State<MapScreen> {
+  _MapScreenState(double long, double lat);
   GoogleMapController? mapController; //contrller for Google map
   Set<Marker> markers = Set(); //markers for google map
-  LatLng showLocation = LatLng(14.569763, -90.559597);
+
+  static double get lat => _MapScreenState.lat;
+
+  static double get long => _MapScreenState.long;
+
+  LatLng showLocation = LatLng(long, lat);
 
   @override
   void initState() {
